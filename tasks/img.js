3b5bcfa6ef3be6352068bd80adc071103de71045
cyber-wf13 <o.zon77788@gmail.com>
2024-05-27 20:30:49 +0300

    Update dir structure and translate REAMDE

--- tasks/img.js
+++
@@ -1,8 +0,0 @@
-const {src,	dest} = require('gulp');
-const bs = require('browser-sync');
-
-module.exports = function img() {
-	return src('src/img/**/*.+(png|jpg|jpeg|gif|svg|ico)')
-		.pipe(dest('dist/img'))
-  	.pipe(bs.stream())
-}
